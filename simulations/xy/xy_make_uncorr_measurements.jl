21c90ea2bfc2e737ec7a68909ec07d92afa6a03d
JaydevSR <jaydevsr7701@gmail.com>
2022-03-16 10:41:23 +0530

    new measurements for xy

--- simulations/xy/xy_make_uncorr_measurements.jl
+++ simulations/xy/xy_make_uncorr_measurements.jl
@@ -1,11 +1,11 @@
 include("../../src/spinmc.jl")
 
-file = jldopen("results/xy/xy_config_data.jld2", "w+")
+file = jldopen("results/xy/xy_config_data.jld2", "w+"; compress=true)
 
 Temps = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6]
 τvals = [20 , 25 , 30 , 35 , 45 , 55 , 65 , 75 , 85 , 95 , 115, 145]
 
-lattice_sizes = [40] # [10, 20, 30]
+lattice_sizes = [10, 20, 30, 40]
 eqsteps = 2000
 n_uncorr = 1000
 
