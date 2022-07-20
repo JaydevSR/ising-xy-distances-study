63f50610c2d76ef512e890b554597f14cca77a5a
JaydevSR <jaydevsr7701@gmail.com>
2022-07-20 11:43:59 +0530

    add env, add gitignore

--- src/spinmc.jl
+++ src/spinmc.jl
@@ -3,12 +3,18 @@ using DelimitedFiles
 using Random
 using StaticArrays
 
+include("lazystack.jl")
+
 abstract type SpinModel2D{T} end
 
 function get_neighbors(model::SpinModel2D, k::CartesianIndex)
     ns = length(model.shifts)
     return ntuple(i -> CartesianIndex(mod1.((k+model.shifts[i]).I, model.L)), ns)
 end
+
+include("ising.jl")
+
+include("xy.jl")
 
 function cos2pi(x::Float64)
     return cospi(2x)
@@ -18,7 +24,4 @@ function sin2pi(x::Float64)
     return sinpi(2x)
 end
 
-include("lazystack.jl")
-include("ising.jl")
-include("xy.jl")
 inclure("statutils.jl")
